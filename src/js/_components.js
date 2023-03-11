5364c34da0595524fae14b36c1ce2172645b6fb0
Pavel-Aleksandrovich-1 <pavelaleksandrovichgid@gmail.com>
2023-03-11 16:10:29 +0700

    ABOUT

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,4 +1,6 @@
 import "../js/components/smooth-scroll"
 import "../js/components/slider"
 import "../js/components/video"
+import "../js/components/validation"
+
 
---
+++ src/js/components/validation.js
@@ -0,0 +1,42 @@
+import { validateForms } from '../functions/validate-forms';
+
+const rules = [
+  {
+    ruleSelector: '.input-name',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните имя!'
+      }
+    ]
+  },
+  {
+    ruleSelector: '.input-tel',
+    tel: true,
+    telError: 'Введите корректный телефон',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните телефон!'
+      }
+    ]
+  },
+  {
+    ruleSelector: '.input-masseg"',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните поле!'
+      }
+    ]
+  },
+];
+
+const afterForm = () => {
+  console.log('Произошла отправка, тут можно писать любые действия');
+};
+
+validateForms('.order__form', rules, afterForm);
